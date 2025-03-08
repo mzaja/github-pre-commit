8698db49c12f9e260653166f13f0dfabe0704c7b
mzaja <64711255+mzaja@users.noreply.github.com>
2025-03-08 05:17:02 +0000

    Remove temp file use in test script, prevent accidental branch deletion

--- test.sh
+++ test.sh
@@ -15,35 +15,39 @@ valid_commit_msg="#2325 some msg"
 valid_branch="2325-test-branch"
 invalid_commit_msg="#123 msg"
 invalid_branch="test-branch"
-output_file="/tmp/try_repo_test_output"
 
 current_branch="$(git branch --show-current)"
 
 # Check that we are not currently on a test branch
 if [ "$current_branch" == "$valid_branch" ] || [ "$current_branch" == "$invalid_branch" ]; then
-    echo "Cannot run qualification tests on one of the test branches. Please select a different branch name."
+    echo "Cannot run qualification tests on one of the test branches. Please change the branch."
     exit 1
 fi
 
+# Runs the pre-commit hook using try-repo command and inspects the output for the expected result.
 # Arguments:
 # 1) Commit message
 # 2) Branch name
 # 3) "Passed" or "Failed"
 try_repo () {
+    # Safety mechanism to prevent deleting non-test branches
+    if [ "$2" != "$valid_branch" ] && [ "$2" != "$invalid_branch" ]; then
+        echo "Test branch must use one of the approved test branch names."
+        exit 1
+    fi
     echo
     echo "==============================================================================="
     echo "Testing with commit message '$1' on branch '$2'"
     echo "==============================================================================="
-    if [ ! -z "$2" ] && [ "$2" != "main" ]; then
-        # It is easier to run these two commands and suppress errors than it is to test for conditions and avoid errors
-        git switch -d "$current_branch" > /dev/null 2>&1
-        git branch -d "$2" > /dev/null 2>&1 || true
-        git checkout -b "$2" > /dev/null 2>&1 || true
-    fi
+    git switch "$current_branch" > /dev/null 2>&1
+    # It is easier to run these two commands and suppress errors than
+    # it is to test for conditions and avoid errors
+    git branch -d "$2" > /dev/null 2>&1 || true
+    git switch -c "$2" > /dev/null 2>&1 || true
     echo "$1" > "$commit_msg_file"
-    # It is preferrable to redirect the output to a file using tee instead of a subshell output to a variable because tee prints progress to terminal
-    pre-commit try-repo . github-issue-number --verbose --hook-stage commit-msg --commit-msg-filename "$commit_msg_file" | tee "$output_file" || true
-    grep "$hook_name.*$3" "$output_file"
+    output="$(pre-commit try-repo . github-issue-number --verbose --color never \
+            --hook-stage commit-msg --commit-msg-filename "$commit_msg_file" | tee /dev/tty || true)"
+    echo "$output" | grep "$hook_name\.\+$3"
 }
 
 # Restore the original branch on script's exit and delete temp branches
